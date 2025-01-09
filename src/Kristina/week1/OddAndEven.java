c1f59cd96c8e6546b102101575cf85e4b9869855
krist <kristinaks93@gmail.com>
2025-01-09 18:18:13 -0500

    week 1 task completed

--- src/Kristina/week1/OddAndEven.java
+++ src/Kristina/week1/OddAndEven.java
@@ -1,8 +1,25 @@
 package Kristina.week1;
 
-public class OddAndEven {
-    public static void main(String[] args) {
 
+public class OddAndEven {
+
+        //Write  a method which can identify that a  given number is even or odd
         //solution
+
+    public static void oddOrEven(int number){
+
+        if(number%2==0){
+            System.out.println(number + " is an even number");
+        }else{
+            System.out.println(number + " is an odd number");
+        }
     }
+
+    public static void main(String[] args) {
+        oddOrEven(5);
+        oddOrEven(2);
+    }
+
+
+
 }
