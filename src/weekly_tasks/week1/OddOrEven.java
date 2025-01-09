1ad7c1fdc597c41998db9cbcf8d8f4f8951f00e7
DavidCydeo <david@cydeo.com>
2025-01-10 01:17:01 +0300

    Task1 uploaded

--- src/weekly_tasks/week1/OddOrEven.java
+++ src/weekly_tasks/week1/OddOrEven.java
@@ -3,12 +3,12 @@ package weekly_tasks.week1;
 public class OddOrEven {
     public static void main(String[] args) {
 
-        // solution:
-        //second step
+
+        //Write  a method which can identify that a  given number is even or odd
 
 
-
-
+        // identifyOddEven(5);
+        // identifyOddEven(6);
 
 
     }
