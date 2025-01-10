f1b27f70f65be62b72a866d0f9581fb288c515a2
Zlatik73 <krista3mbond@gmail.com>
2025-01-09 22:23:26 -0500

    Zlata.week1.solution

--- src/weekly_tasks/week1/OddOrEven.java
+++ src/weekly_tasks/week1/OddOrEven.java
@@ -3,6 +3,7 @@ package weekly_tasks.week1;
 public class OddOrEven {
 
 
+
     public static void main(String[] args) {
         //Write  a method which can identify that a  given number is even or odd
 
