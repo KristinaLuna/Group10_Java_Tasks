c4b3350a1c3d5d43ad166dca28687c12280b6173
Zlatik73 <krista3mbond@gmail.com>
2025-01-09 21:09:23 -0500

    Zlata.week1.solution

--- src/weekly_tasks/week1/OddOrEven.java
+++ src/weekly_tasks/week1/OddOrEven.java
@@ -1,12 +1,19 @@
 package weekly_tasks.week1;
 
 public class OddOrEven {
+
+
+    public static int OddOrEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Number " + n + " is even.");
+        }else{
+            System.out.println("Number " + n + " is odd.");
+        }
+        return n;
+    }
     public static void main(String[] args) {
-
-        // solution of task 7
-
-
-
+        OddOrEven(7);
+        OddOrEven(426);
 
 
 
