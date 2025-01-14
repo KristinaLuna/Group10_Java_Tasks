467af7e75bc567d3aacd25f1289715a0129590be
RegineLucas <rrclucas@yahoo.com>
2025-01-14 12:55:48 -0800

    oddOrEven Regine's Solution

--- src/Regine/week1/oddOrEven.java
+++ src/Regine/week1/oddOrEven.java
@@ -2,9 +2,19 @@ package Regine.week1;
 
 public class oddOrEven {
 
-    public static void main(String[] args) {
-        //SOLVED - solution below
+    public static void main(String[] args)
 
-        //mine
+        {
+            oddOrEven(99);
+        }
+
+
+        public static void oddOrEven(int num){
+            if (num % 2 == 0) {
+                System.out.println(num + " = number is even");
+            } else {
+                System.out.println(num + " = number is odd");
+            }
+        }
+
     }
-}
