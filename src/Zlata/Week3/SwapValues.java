acdbf5cc52752885744e29774b9e4ea9927341b9
Zlatik73 <krista3mbond@gmail.com>
2025-01-30 00:22:53 -0500

    Zlata.week3.solution

--- src/Zlata/Week3/SwapValues.java
+++ src/Zlata/Week3/SwapValues.java
@@ -2,6 +2,7 @@ package Zlata.Week3;
 
 public class SwapValues {
     public static void main(String[] args) {
+
         int a=10, b=45;
         System.out.println("Original number a = " + a);
         System.out.println("Original number b = " + b);
