001155efdc8b6e6e264dedad75cfbfbe684a7ea4
RegineLucas <rrclucas@yahoo.com>
2025-01-29 16:18:48 -0800

    Swapping values Solution

--- src/Regine/Week3/SwappingValues.java
+++ src/Regine/Week3/SwappingValues.java
@@ -16,7 +16,7 @@ public class SwappingValues {
         n2 = n2 - n1;
 
         System.out.println("n1 = " + n1);
-        System.out.println("n2 = " + n2);
+        System.out.println("n2 =  " + n2);
 
     }
 
