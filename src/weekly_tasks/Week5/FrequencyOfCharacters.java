a706d4c9b35641bfdfaea58797896f422ffec74a
DavidCydeo <david@cydeo.com>
2025-02-06 23:22:25 +0300

    Week 5 task added

---
+++ src/weekly_tasks/Week5/FrequencyOfCharacters.java
@@ -0,0 +1,8 @@
+package weekly_tasks.Week5;
+
+public class FrequencyOfCharacters {
+
+      /*Write a return method that can find the frequency of characters in String
+    Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2*/
+
+}
