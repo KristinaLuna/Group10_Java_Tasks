493c26a70f2d28df15a19a766bb8d531af6c2784
Zlatik73 <krista3mbond@gmail.com>
2025-02-13 14:27:32 -0500

    Zlata.week5.solution

---
+++ src/Zlata/Week5/FrequencyOfCharacters.java
@@ -0,0 +1,19 @@
+package Zlata.Week5;
+
+public class FrequencyOfCharacters {
+    public static String frequencyOfCharacters(String inputStr) {
+        String result = ""; //to store a result of method
+
+        while(!inputStr.isEmpty()) { //iterates till inputlStr reach 0 characters
+            char ch = inputStr.charAt(0); //retrieves the first char
+            int count = inputStr.length() - inputStr.replace(String.valueOf(ch), "").length();  //calculate number of same ch characters in inputStr
+            result = result + ch + count; //to result adds ch and number of ch
+            inputStr = inputStr.replace(String.valueOf(ch), ""); //removes all ch from inputStr
+        }
+        return result; //Returns the final result
+    }
+    public static void main(String[] args) {
+        System.out.println(frequencyOfCharacters("AAABBCDD")); //Output A3B2C1D2
+        System.out.println(frequencyOfCharacters("ZZZZLLLATIKKKk")); //Output Z4L3A1T1I1K3k1
+    }
+}
